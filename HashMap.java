1bfbc775d2ab298e67e07e09cfd231035e6ab013
amritsinchan <57107663+amritsinchan@users.noreply.github.com>
2021-04-03 17:01:14 +0530

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -85,3 +85,30 @@ public static void positionofallchars(String str){
     }
 
 }
+
+
+top k frequent
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map= new HashMap<>();
+        for(int ele: nums)
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b)->{
+            
+        });return a[1] - b[1];
+        for(Integer e: map keySet())) {
+            int val= e;
+            int freq= map.get(val);
+            int[] arr= new int[]{ val, freq};
+            pq.add(arr);
+            if(pq.size()>k)
+                pq.remove();
+        }
+        int ans= new int[k];
+        int idx=0;
+        while(pq!size()!=0){
+            ans[idx++]= pq.remove()[0];
+        }
+        return ans;
+    }
+}
